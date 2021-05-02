5f79facbce13f6346ecafe13a04272a15df84209
noah <nomaxx117@gmail.com>
2021-05-02 11:37:23 -0500

    No longer need to install the cpp libs first via sudo.

--- build.rs
+++ build.rs
@@ -4,7 +4,8 @@ fn main() {
 
     let dst = cmake::Config::new("cameralot")
         .generator("Ninja")
-        .build();
+        .build()
+        .join("build");
 
     println!("cargo:rustc-link-search=native={}", dst.display());
     println!("cargo:rustc-link-lib=static=cameralot-capture");
